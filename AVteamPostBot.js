d8ad67a0e07dd860e3a7667e4b958d4663b1e555
NouP1 <smolen43ret@gmail.com>
2024-08-03 12:07:27 +0300

    update f sr

--- AVteamPostBot.js
+++ AVteamPostBot.js
@@ -182,7 +182,7 @@ app.get('/postback', async (req, res) => {
     }
 });
 
-cron.schedule('0 5 15 * * *', () => {
+cron.schedule('0 0 21 * * *', () => {
     const now = moment().format('YYYY-MM-DD HH:mm:ss')
     console.log('Running resetRevenueCount at 00:00');
     resetRevenueCount();
