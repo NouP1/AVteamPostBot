bf56b5110e7855184a20aaab4725cb19d7b0e7e7
NouP1 <smolen43ret@gmail.com>
2025-02-21 14:18:06 +0300

    update aff

--- data/affneworks.js
+++ data/affneworks.js
@@ -31,7 +31,7 @@ const affiliateNetworkMapping = {
     'Partners 30': 'Clicklead',
     'Partners 31': 'Profitov',
     'Partners 32': 'Trafflab',
-    'Partners 33': 'ChillipartnersBudget',
+    'Partners 33': '22bet',
     
 };
 
